7a21978f1e4dfeeee93a2a17d405ecb9100b4570
Stuart Bowes <stuartbowes2000@gmail.com>
2022-08-12 13:25:18 +0100

    Add Container Transform transition from Floating Action Button to compose email page

--- lib/home.dart
+++ lib/home.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 import 'package:reply/model/router_provider.dart';
+import 'package:animations/animations.dart';
 
 import 'bottom_drawer.dart';
 import 'colors.dart';
@@ -731,41 +732,42 @@ class _ReplyFabState extends State<_ReplyFab>
         final tooltip = onMailView ? 'Reply' : 'Compose';
 
         // TODO: Add Container Transform from FAB to compose email page (Motion)
-        return Material(
-          color: theme.colorScheme.secondary,
-          shape: circleFabBorder,
-          child: Tooltip(
-            message: tooltip,
-            child: InkWell(
-              customBorder: circleFabBorder,
-              onTap: () {
-                Provider.of<EmailStore>(
-                  context,
-                  listen: false,
-                ).onCompose = true;
-
-                Navigator.of(context).push(
-                  PageRouteBuilder(
-                    pageBuilder: (
-                      BuildContext context,
-                      Animation<double> animation,
-                      Animation<double> secondaryAnimation,
-                    ) {
-                      return const ComposePage();
-                    },
+        return OpenContainer(
+            openBuilder: (context, closedContainer) {
+              return const ComposePage();
+            },
+            openColor: theme.cardColor,
+            onClosed: (success) {
+              Provider.of<EmailStore>(
+                context,
+                listen: false,
+              ).onCompose = false;
+            },
+            closedShape: circleFabBorder,
+            closedColor: theme.colorScheme.secondary,
+            closedElevation: 6,
+            closedBuilder: (context, openContainer) {
+              return Tooltip(
+                message: tooltip,
+                child: InkWell(
+                  customBorder: circleFabBorder,
+                  onTap: () {
+                    Provider.of<EmailStore>(
+                      context,
+                      listen: false,
+                    ).onCompose = true;
+                    openContainer();
+                  },
+                  child: SizedBox(
+                    height: _mobileFabDimension,
+                    width: _mobileFabDimension,
+                    child: Center(
+                      child: fabSwitcher,
+                    ),
                   ),
-                );
-              },
-              child: SizedBox(
-                height: _mobileFabDimension,
-                width: _mobileFabDimension,
-                child: Center(
-                  child: fabSwitcher,
                 ),
-              ),
-            ),
-          ),
-        );
+              );
+            });
       },
     );
   }
